5e46d00a3edc70bb2e96c324721c95f02eb6c3f2
Jo Jauch <danieljojauch@gmail.com>
2025-05-10 01:07:21 +0200

    Nochmal eine kleine Änderung

--- TuDo_app/main.py
+++ TuDo_app/main.py
@@ -1,26 +1,29 @@
 ################################## TuDu_App  ##########################################
-# main.py  ojo
+# Praxis Projekt To Do Liste
 
-# main() Startet streamlit:
-# sucht nach der streamlit Anwendung im Pfad als Failsafe
+# main.py  
 
-# streamlit run tudu_app.py
+# Startet streamlit:
+# sucht nach der streamlit Anwendung im Pfad als Failsafe (shutil.which)
+
 
 import os
 import subprocess
 import shutil
 
 def start_streamlit():
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    tudu_root = os.path.abspath(os.path.join(base_dir, ".."))  # TuDu/
+    base_dir = os.path.dirname(os.path.abspath(__file__))  # absoluter Pfad zu main.py
+    tudu_root = os.path.abspath(os.path.join(base_dir, ".."))  # geht hoch in Ordner TuDu/
     script_path = os.path.join(tudu_root, "app", "tudu_app.py")
-    # script_path = os.path.join(base_dir, "app", "tudu_app.py")
-    streamlit_exe = shutil.which("streamlit")
+     
+    streamlit_exe = shutil.which("streamlit")  
+    # CHeck ob streamlit.exe im Systempfad verfügbar ist, 
+    # bzw sucht im Systempfad nach der streamlit-Befehlszeile (streamlit.exe)
 
     if streamlit_exe is None:
-        raise RuntimeError("Streamlit wurde nicht gefunden. Ist es in der Umgebung installiert?")
+        raise RuntimeError("Streamlit wurde nicht gefunden!")  # wenn streamlit fehlt
 
-    subprocess.run([streamlit_exe, "run", script_path])
+    subprocess.run([streamlit_exe, "run", script_path])  # startet streamlit wie im Terminal
 
 if __name__ == "__main__":
     start_streamlit()
